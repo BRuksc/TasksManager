97ff125f810a810dd486de9f09c998c3ffd792c4
Bogumił Rukść <rukscbogumil@gmail.com>
2024-08-09 12:38:11 +0200

    Adding TasksHistory table

---
+++ TasksManager/TasksManager.Database/Tables/TasksHistory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TasksHistory]
+(
+	[Id] UNIQUEIDENTIFIER UNIQUE DEFAULT NEWSEQUENTALID() NOT NULL,
+	[TasksHistoryId] INT UNIQUE IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[TaskId] UNIQUEIDENTIFIER NOT NULL 
+		FOREIGN KEY (TaskId) REFERENCES dbo.Tasks(Id),
+	[Column] NVARCHAR(20) NOT NULL
+		CONSTRAINT [Column] CHECK ([Column] = 'Name' OR [Column] = 'Description' 
+		OR [Column] = 'Owner' OR [Column] = 'Shared' OR [Column] = 'Priority' 
+		OR [Column] = 'StoryPointsEstimation'),
+	[NewValue] NVARCHAR(MAX) NOT NULL,
+	[OldValue] NVARCHAR(MAX) NOT NULL
+)
