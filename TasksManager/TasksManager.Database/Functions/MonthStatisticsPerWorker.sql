ef2c53dafe40b16a4dccebe6586fd1b10a4f6f29
Bogumił Rukść <rukscbogumil@gmail.com>
2024-08-10 10:20:34 +0200

    Adding MonthStatisticsPerWorker function, editing an one column definition in the TasksHistory table

---
+++ TasksManager/TasksManager.Database/Functions/MonthStatisticsPerWorker.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[MonthEditPerWorker] (@workerId UNIQUEIDENTIFIER)
+RETURNS TABLE
+AS
+	RETURN SELECT 
+    t.Owner AS WorkerId, 
+    COUNT(th.TaskId) AS ChangeCount
+    FROM 
+        dbo.Tasks t
+    INNER JOIN 
+        dbo.TasksHistory th ON t.Id = th.TaskId
+    WHERE 
+        th.DateOfChange >= DATEADD(MONTH, -1, GETDATE())
+        AND th.DateOfChange < GETDATE()
+        AND th.OperationType = 'Resolving'
+    GROUP BY 
+        t.Owner;
--- TasksManager/TasksManager.Database/Tables/TasksHistory.sql
+++ TasksManager/TasksManager.Database/Tables/TasksHistory.sql
@@ -2,18 +2,18 @@
 (
     [Id] UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID() NOT NULL PRIMARY KEY,
 	[TasksHistoryId] INT UNIQUE IDENTITY(1,1) NOT NULL,
-	[TaskId] UNIQUEIDENTIFIER NOT NULL 
-		FOREIGN KEY (TaskId) REFERENCES dbo.Tasks(Id),
+	[TaskId] UNIQUEIDENTIFIER NOT NULL,
 	[OperationType] NVARCHAR(20) NOT NULL
 		 CONSTRAINT CHK_OperationType CHECK (
 			OperationType = 'Deletion' OR
 			OperationType = 'Editing' OR 
-			OperationType = 'Adding'),
+			OperationType = 'Adding' OR
+			OperationType = 'Resolving'),
 	[Column] NVARCHAR(20) NOT NULL
 		CONSTRAINT [CHK_Column] CHECK ([Column] = 'Name' OR [Column] = 'Description' 
 		OR [Column] = 'Owner' OR [Column] = 'Shared' OR [Column] = 'Priority' 
 		OR [Column] = 'StoryPointsEstimation'),
-	[NewValue] NVARCHAR(MAX) NOT NULL,
-	[OldValue] NVARCHAR(MAX) NOT NULL,
-	[DataOfChange] DATETIME NOT NULL
+	[NewValue] NVARCHAR(MAX) NULL,
+	[OldValue] NVARCHAR(MAX) NULL,
+	[DateOfChange] DATETIME NOT NULL
 )
