9ca90006cda121b5878b00c8d9e9069a8be519e7
Bogumił Rukść <rukscbogumil@gmail.com>
2024-08-10 10:44:30 +0200

    Adding trigger for verification ManagerId value.

--- TasksManager/TasksManager.Database/Tables/Users.sql
+++ TasksManager/TasksManager.Database/Tables/Users.sql
@@ -4,8 +4,9 @@
 	[UserId] INT UNIQUE IDENTITY(1,1) NOT NULL,
 	[Name] VARCHAR(50) NOT NULL,
 	[Surname] VARCHAR(50) NOT NULL,
-	[ManagerId] UNIQUEIDENTIFIER NOT NULL,
 	[Position] NVARCHAR(50) NOT NULL
 		CONSTRAINT [Position] 
-		CHECK ([Position] = 'Worker' OR [Position] = 'Manager')
+		CHECK ([Position] = 'Worker' OR [Position] = 'Manager'),
+	[ManagerId] UNIQUEIDENTIFIER NULL 
+		CONSTRAINT [ManagerId] CHECK ([Position] = 'Worker')
 )
---
+++ TasksManager/TasksManager.Database/Triggers/ManagerIdVerification.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER [ManagerIdVerification]
+	ON dbo.Users
+	AFTER INSERT, UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (
+		SELECT Id FROM dbo.Users WHERE ManagerId NOT IN (
+			SELECT Id FROM dbo.Users WHERE Position = 'Manager'
+		))
+	BEGIN
+		DELETE FROM dbo.Users WHERE ManagerId NOT IN (
+			SELECT Id FROM dbo.Users WHERE Position = 'Manager'
+		)
+	END
+END
