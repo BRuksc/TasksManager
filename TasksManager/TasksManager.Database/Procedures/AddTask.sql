0e9d0c97ae0b261b721da51cbff6d62584659ebc
Bogumił Rukść <rukscbogumil@gmail.com>
2024-08-11 19:35:11 +0200

    Edit TaskChanged and AddTask

--- TasksManager/TasksManager.Database/Procedures/AddTask.sql
+++ TasksManager/TasksManager.Database/Procedures/AddTask.sql
@@ -1,16 +1,19 @@
 ﻿CREATE PROCEDURE [dbo].[AddTask]
-	@Name NVARCHAR(200) NOT NULL,
+	@Name NVARCHAR(200) NULL,
 	@Description NVARCHAR(MAX) NULL,
 	@Owner UNIQUEIDENTIFIER NULL,
-	@Shared BIT NOT NULL,
-	@Priority INT NOT NULL,
+	@Shared BIT NULL,
+	@Priority INT NULL,
 	@StoryPointsEstimation INT NULL
 AS
 	BEGIN TRY
-		INSERT INTO [dbo].[Tasks]
-			(Name, Description, Owner, Shared, Priority, StoryPointsEstimation)
-		VALUES
-			(@Name, @Description, @Owner, @Shared, @Priority, @StoryPointsEstimation)
+		IF (@Name IS NOT NULL AND @Shared IS NOT NULL AND @Priority IS NOT NULL)
+		BEGIN
+			INSERT INTO [dbo].[Tasks]
+				(Id, Name, Description, Owner, Shared, Priority, StoryPointsEstimation)
+			VALUES
+				(NEWID(), @Name, @Description, @Owner, @Shared, @Priority, @StoryPointsEstimation)
+		END
 	END TRY
 	BEGIN CATCH
 		THROW;
--- TasksManager/TasksManager.Database/Triggers/TaskChanged.sql
+++ TasksManager/TasksManager.Database/Triggers/TaskChanged.sql
@@ -1,87 +1,84 @@
 ﻿CREATE TRIGGER [TaskChanged]
-	ON [dbo].[Tasks]
-	AFTER UPDATE, INSERT, DELETE
-	AS
-	BEGIN
-		BEGIN TRY
+ON [dbo].[Tasks]
+AFTER UPDATE, INSERT, DELETE
+AS
+BEGIN
+    BEGIN TRY
+        SET NOCOUNT ON;
 
-			SET NOCOUNT ON
+        DECLARE @columnValue NVARCHAR(50) = '';
+        DECLARE @oldValue NVARCHAR(MAX);
+        DECLARE @newValue NVARCHAR(MAX);
 
-			DECLARE @columnValue NVARCHAR(50)
-			DECLARE @columnValueWasChanged BIT
+        IF UPDATE(Name) 
+        BEGIN
+            SET @columnValue += 'Name';
+        END
+        IF UPDATE(Description) 
+        BEGIN
+            IF LEN(@columnValue) > 0 SET @columnValue += ', ';
+            SET @columnValue += 'Description';
+        END
+        IF UPDATE(Owner)
+        BEGIN
+            IF LEN(@columnValue) > 0 SET @columnValue += ', ';
+            SET @columnValue += 'Owner';
+        END
+        IF UPDATE(Shared)
+        BEGIN
+            IF LEN(@columnValue) > 0 SET @columnValue += ', ';
+            SET @columnValue += 'Shared';
+        END
 
-			DECLARE @oldValue NVARCHAR(50)
-			DECLARE @newValue NVARCHAR(50)
-
-			IF UPDATE(Name) 
-			BEGIN
-				SET @columnValue = 'Name'
-				SET @columnValueWasChanged = 1
-			END
-			IF UPDATE(Description) 
-			BEGIN
-				IF @columnValueWasChanged = 1 SET @columnValue += ', '
-				SET @columnValue += 'Description'
-				SET @columnValueWasChanged = 1
-			END
-			IF UPDATE(Owner)
-			BEGIN
-				IF @columnValueWasChanged = 1 SET @columnValue += ', '
-				SET @columnValue += 'Owner'
-				SET @columnValueWasChanged = 1
-			END
-			IF UPDATE(Shared)
-			BEGIN
-				IF @columnValueWasChanged = 1 SET @columnValue += ', '
-				SET @columnValue += 'Shared'
-			END
-
-			IF EXISTS (SELECT * FROM deleted) AND EXISTS (SELECT * FROM inserted)
-			BEGIN
-				INSERT INTO [dbo].[TasksHistory] 
-					([TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
-					SELECT
-						i.TaskId,
-						@columnValue,
-						'Editing',
-						@newValue,
-						@oldValue,
-						GETDATE()
-					FROM
-						inserted i 
-					WHERE 
-						EXISTS (SELECT * FROM deleted)
-			  END
-
-			  ELSE IF EXISTS (SELECT * FROM deleted)
-			  BEGIN
-				INSERT INTO [dbo].[TasksHistory] ([TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
-					SELECT
-						d.TaskId,
-						@columnValue,
-						'Deletion',
-						@newValue,
-						@oldValue,
-						GETDATE()
-					FROM
-						deleted d 
-			  END
-
-			  ELSE
-			  BEGIN
-				INSERT INTO [dbo].[TasksHistory] ([TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
-					SELECT
-						i.TaskId,
-						@columnValue,
-						'Adding',
-						@newValue,
-						@oldValue,
-						GETDATE()
-					FROM
-						inserted i 
-			  END
-		END TRY
-		BEGIN CATCH
-			THROW;
-		END CATCH
-	END
+        IF EXISTS (SELECT * FROM deleted) AND EXISTS (SELECT * FROM inserted)
+        BEGIN
+            INSERT INTO [dbo].[TasksHistory] 
+                ([Id], [TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
+                SELECT
+                    NEWID(),
+                    i.Id,
+                    @columnValue,
+                    'Editing',
+                    CASE WHEN i.Name IS NOT NULL THEN i.Name ELSE '' END,
+                    CASE WHEN d.Name IS NOT NULL THEN d.Name ELSE '' END,
+                    GETDATE()
+                FROM
+                    inserted i 
+                JOIN
+                    deleted d ON i.Id = d.Id;
+        END
+        ELSE IF EXISTS (SELECT * FROM deleted)
+        BEGIN
+            INSERT INTO [dbo].[TasksHistory] 
+                ([Id], [TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
+                SELECT
+                    NEWID(),
+                    d.Id,
+                    @columnValue,
+                    'Deletion',
+                    '',
+                    d.Name,
+                    GETDATE()
+                FROM
+                    deleted d;
+        END
+        ELSE
+        BEGIN
+            INSERT INTO [dbo].[TasksHistory] 
+                ([Id], [TaskId], [Column], [OperationType], [NewValue], [OldValue], [DateOfChange])
+                SELECT
+                    NEWID(),
+                    i.Id,
+                    @columnValue,
+                    'Adding',
+                    i.Name,
+                    '',
+                    GETDATE()
+                FROM
+                    inserted i;
+        END
+    END TRY
+    BEGIN CATCH
+        THROW;
+    END CATCH
+END;
